2de601e3ebf2e62546be2c6bfe78433e6905fde7
seewe <eebouele@yahoo.fr>
2021-11-29 23:08:15 -0500

    Commit du projet

--- setUp.R
+++ setUp.R
@@ -1,10 +1,20 @@
 #install.packages("blogdown")
 library(blogdown)
 
+#'
+#'Instalation de HuGO pour acc.der aux themes publiés sur HUGO
+#'
 blogdown::install_hugo()
 
 
+#'
+#' Selection d'un theme hugo et cloner dans le projet
+#'
 
+blogdown::new_site(theme = "StaticMania/portio-hugo")
+blogdown::new_site(theme = "themefisher/kross-hugo")
+blogdown::new_site(theme = "syui/hugo-theme-air")
+blogdown::new_site(theme = "somratpro/somrat")
 
 
 
